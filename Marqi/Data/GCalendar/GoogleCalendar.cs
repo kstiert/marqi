94078b8ef65b796ba9184f09f853ffe156af138e
Devin Stiert <devin.stiert@thelevelup.com>
2019-05-17 21:17:08 -0400

    Fix calendar display

--- Marqi/Data/GCalendar/GoogleCalendar.cs
+++ Marqi/Data/GCalendar/GoogleCalendar.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using Ical.Net;
 using Ical.Net.CalendarComponents;
+using Ical.Net.DataTypes;
 
 namespace Marqi.Data.GCalendar
 {
@@ -16,23 +17,22 @@ namespace Marqi.Data.GCalendar
             var calResp = await new HttpClient().GetAsync("https://calendar.google.com/calendar/ical/j822eaksjlh7ktnemt509pu95k%40group.calendar.google.com/private-2fcb409c2dd5a289303adff532f42a4f/basic.ics");
             var cal = Calendar.Load(calResp.Content.ReadAsStreamAsync().Result);
             var events = cal.GetOccurrences(DateTime.Today, DateTime.Today.AddDays(7))
-                            .Select(o => o.Source)
-                            .Cast<CalendarEvent>()
-                            .OrderBy(e => e.Start)
+                            .OrderBy(o => o.Period.StartTime)
                             .Select(MakeEvent).ToList();
             Update(events);
         }
 
-        public GoogleCalendarEvent MakeEvent(CalendarEvent ev)
+        public GoogleCalendarEvent MakeEvent(Occurrence o)
         {
-            var start = ev.Start.Value.ToLocalTime();
-            var end = ev.End.Value.ToLocalTime();
+            var ev = o.Source as CalendarEvent;
+            var start = o.Period.StartTime.Value;
+            var end = o.Period.EndTime.Value;
             var datetime = start.ToString("M/d");
             if (ev.IsAllDay)
             {
                 if (ev.Duration > TimeSpan.FromDays(1))
                 {
-                    datetime += $"-{end.ToString("M/d")}";
+                    datetime += $"-{end.AddDays(-1).ToString("M/d")}";
                 }
             }
             else
