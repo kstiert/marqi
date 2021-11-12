3b4d9a94c38f8741e645d89386a4798ff67627de
Devin Stiert <devin.stiert@gmail.com>
2021-11-12 13:30:32 -0500

    todoist support

---
+++ Marqi/Data/Todoist/Todo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Marqi.Data.Todoist
+{
+    public class Todo
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Marqi/Data/Todoist/TodoProject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Todoist.Net;
+
+namespace Marqi.Data.Todoist
+{
+    public class TodoProject : IDataSource<List<Todo>>
+    {
+        private readonly ITodoistClient _client;
+
+        public TodoProject()
+        {
+            _client = new TodoistClient("62575f21e12cb64ef291e8716f63c8edc117000a");
+        }
+
+        public Action<List<Todo>> Update { get; set; }
+
+        public async Task Refresh()
+        {
+            var items = await _client.Items.GetAsync();
+            Update(items.Select(i => new Todo { Name = i.Content }).ToList());
+        }
+    }
+}
