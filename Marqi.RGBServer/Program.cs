51edf20d37e60f520fd26eef38ce40eb2a9f7470
devin <devin.stiert@gmail.com>
2022-09-04 14:09:32 -0400

    cleanup

--- Marqi.RGBServer/Program.cs
+++ Marqi.RGBServer/Program.cs
@@ -13,33 +13,16 @@ namespace Marqi.RGBServer
         {
             var fontFactory = new RGBFontFactory();
             var display = new RGBDisplay(fontFactory);
-            //var queue = new DisplayQueue();
 
-            var rcpServer = new NpHost(Constants.RGB_PIPE, new ServiceWireLogger());
-            rcpServer.AddService<IFontFactory>(fontFactory);
-            rcpServer.AddService<IDisplay>(display);
-            rcpServer.Open();
+            var rpcServer = new NpHost(Constants.RGB_PIPE, new ServiceWireLogger());
+            rpcServer.AddService<IFontFactory>(fontFactory);
+            rpcServer.AddService<IDisplay>(display);
+            rpcServer.Open();
 
-            /*
-            Action<IDisplay> action;
-
-            while(!Console.KeyAvailable)
-            {
-                if(queue.Queue.TryDequeue(out action))
-                {
-                    //display.DrawLine(0,0,32,32, new Color(255,255,255));
-                    action(display);
-                }
-                else
-                {
-                    Thread.Sleep(250);
-                }
-            }     
-            */
             Console.WriteLine("Server Open");
             Console.ReadLine();       
 
-            rcpServer.Close();
+            rpcServer.Close();
         }
     }
 }
