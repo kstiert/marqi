8feb7f6626fe0c93aaf74d83e0f40ccd02ed3bb4
devin <devin.stiert@gmail.com>
2022-12-02 13:02:30 -0500

    Handle exceptions in OpenWeather

--- Marqi/Data/Weather/OpenWeather/OpenWeather.cs
+++ Marqi/Data/Weather/OpenWeather/OpenWeather.cs
@@ -4,6 +4,7 @@ using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Marqi.Data.Weather.OpenWeather.Model;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
@@ -12,14 +13,17 @@ namespace Marqi.Data.Weather.OpenWeather
 {
     public class OpenWeather : IDataSource<List<WeatherReport>>
     {
+        private readonly ILogger _logger; 
+
         private readonly IHttpClientFactory _httpFactory;
 
         private readonly string _apiKey;
 
         private readonly List<string> _zips;
 
-        public OpenWeather(IHttpClientFactory httpFactory, IOptions<OpenWeatherOptions> options)
+        public OpenWeather(ILogger<OpenWeather> logger, IHttpClientFactory httpFactory, IOptions<OpenWeatherOptions> options)
         {
+            _logger = logger;
             _httpFactory = httpFactory;
             _apiKey = options.Value.ApiKey;
             _zips = options.Value.Zip;
@@ -31,17 +35,24 @@ namespace Marqi.Data.Weather.OpenWeather
 
         public async Task Refresh()
         {
-            var resp = await _httpFactory.CreateClient().GetAsync($"http://api.openweathermap.org/data/2.5/weather?zip={_zips[0]}&units=imperial&appid={_apiKey}");
-            if (resp.IsSuccessStatusCode)
+            try
             {
-                var content = await resp.Content.ReadAsStringAsync();
-                var weather = JsonSerializer.Deserialize<OpenWeatherResponse>(content);
+                var resp = await _httpFactory.CreateClient().GetAsync($"http://api.openweathermap.org/data/2.5/weather?zip={_zips[0]}&units=imperial&appid={_apiKey}");
+                if (resp.IsSuccessStatusCode)
+                {
+                    var content = await resp.Content.ReadAsStringAsync();
+                    var weather = JsonSerializer.Deserialize<OpenWeatherResponse>(content);
 
-                Update(new List<WeatherReport> { new WeatherReport {
-                    Temperature = weather.main.temp.ToString("F0"),
-                    Location = weather.name,
-                    Icon = Image.Load<Rgba32>($"icons/weather/{weather.weather[0].icon}.png")
-                }});
+                    Update(new List<WeatherReport> { new WeatherReport {
+                        Temperature = weather.main.temp.ToString("F0"),
+                        Location = weather.name,
+                        Icon = Image.Load<Rgba32>($"icons/weather/{weather.weather[0].icon}.png")
+                    }});
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogWarning(e, $"Failed to refresh OpenWeather data for {_zips[0]}");
             }
         }
     }
